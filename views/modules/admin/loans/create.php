d0551a6fa1cc562d86de3d0b4dcc46529d477f77
Ignacio Morinigo <morinacho@gmail.com>
2018-11-16 01:43:22 -0300

    cambios en prestamos

--- views/modules/admin/loans/create.php
+++ views/modules/admin/loans/create.php
@@ -8,15 +8,6 @@
 <div class="col-6">
 	<input type="text" name="bookName" placeholder="Nombre socio">
 </div>
-
-<?php 
-	if (isset($_POST['buscarlibro'])) {
-?>
-	<script type="text/javascript">
-		alert("Hola");
-	</script>
-<?php 
-}
- ?>
+<script type="text/javascript" src=""></script>
 
 	
